08e2613a322d8bf53d8e9f48e4ac9ead09310961
Alexandra <wilderweinundich@gmail.com>
2025-12-03 14:56:29 +0100

    create footer component, about view,  and home view

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+        <nav class="nav-footer">
+            Footer
+        </nav>
+    </footer>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-        <header>
-          <nav class="nav-header">
-              <img alt="logo" src="../assets/img_logo_dtt@3x.png">
-          </nav>
-        </header>
+    <header>
+        <nav class="nav-header">
+            <img alt="logo" src="../assets/img_logo_dtt@3x.png">
+        </nav>
+    </header>
 </template>
