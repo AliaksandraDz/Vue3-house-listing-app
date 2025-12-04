c4ca06eec48589be473dfd776bb0b0541a032e7f
Alexandra <wilderweinundich@gmail.com>
2025-12-04 11:24:01 +0100

    vreate component HouseListing

---
+++ src/components/HouseListing.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="house-listing">
+      <img class="house-listing-image" src="../assets/house10.jpg" alt="House Image">
+        <div class="house-listing-info">
+          <h3>Palissade 110</h3>
+          <p>€ 1000000</p>
+          <p>6478 DH Leerdam</p>
+          <p><img src="../assets/ic_bed@3x.png" alt="Bed" /> 3 <img src="../assets/ic_bath@3x.png" alt="Bath" /> 2 <img src="../assets/ic_size@3x.png" alt="Size" /> 120 m2</p>
+        </div>
+    </div>
+</template>
