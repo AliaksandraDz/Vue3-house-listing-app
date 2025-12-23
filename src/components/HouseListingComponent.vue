2effc6f858a3d46106489d3ae1a604bb860f715e
Alexandra <wilderweinundich@gmail.com>
2025-12-23 16:59:23 +0100

    add edit and delete buttons to the home page

--- src/components/HouseListingComponent.vue
+++ src/components/HouseListingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link :to="{ name: 'HouseDetailsView', params: { id: house.id }}">
+  <router-link :to="{ name: 'HouseDetailsView', query: { delete: clickedOnDelete}, params: { id: house.id }}">
     <article class="house-card">
       <img class="house-card-image" :src="house.image" alt="House"/>
       <div class="house-card-info">
@@ -18,11 +18,23 @@
           </span>
         </p>
       </div>
+      <div class="house-card-actions">
+        <router-link
+          :to="{ name: 'HouseEditView', params: { id: house.id }}"
+          class="btn btn-edit">
+            <img class="btn-edit-icon" src="../assets/ic_edit@3x.png" alt="Edit" />
+        </router-link>
+        <button class="btn btn-delete" @click="clickedOnDelete = true">
+            <img class="btn-delete-icon" src="../assets/ic_delete@3x.png" alt="Delete" />
+        </button>
+      </div>
     </article>
   </router-link>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'HouseListingComponent',
   props: {
@@ -31,5 +43,11 @@ export default {
       required: true,
     },
   },
+
+  setup() {
+    const clickedOnDelete = ref(false)
+
+    return { clickedOnDelete }
+  }
 }
 </script>
